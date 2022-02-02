45b972bacf9e74c2e170fae54d73e1ac6e37f92e
Anton Kliaschonak <PuY25@bk.ru>
2022-02-02 20:32:43 +0300

    feat: add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const closeBracket= Object.fromEntries(bracketsConfig)
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    str[i] === closeBracket[stack[stack.length - 1]]
+        ? stack.pop()
+        : stack.push(str[i]);
+
+  }
+
+  return stack.length === 0;
 }
